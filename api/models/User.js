bc741b1c86112dc7ff16a2199bed4ab99f9d3f19
Sam Bloomberg <sam@redxdev.com>
2015-12-21 14:15:32 -0500

    initial User model

---
+++ api/models/User.js
@@ -0,0 +1,77 @@
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string'
+    },
+
+    email: {
+      type: 'string',
+      unique: true,
+      index: true
+    },
+
+    authMethod: {
+      type: 'string',
+      enum: ['saml', 'google'],
+      defaultsTo: 'saml'
+    },
+
+    apiKey: {
+      type: 'string',
+      size: 32,
+      unique: true,
+      index: true
+    },
+
+    active: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+
+    access: {
+      type: 'string',
+      defaultsTo: 'player'
+    },
+
+    signupDate: {
+      type: 'datetime',
+      defaultsTo: function() {
+        return new Date();
+      }
+    },
+
+    team: {
+      type: 'string',
+      enum: ['human', 'zombie'],
+      defaultsTo: 'human'
+    },
+
+    zombieId: {
+      type: 'string',
+      index: true,
+      unique: true
+    },
+
+    humanIds: {
+      type: 'array',
+      defaultsTo: []
+    },
+
+    humansTagged: {
+      type: 'integer',
+      defaultsTo: 0
+    },
+
+    badges: {
+      type: 'array',
+      defaultsTo: []
+    },
+
+    printed: {
+      type: 'boolean',
+      defaultsTo: false
+    }
+  }
+};
+
