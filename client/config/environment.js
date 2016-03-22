b8995c7235f2e631eab68ef82abdb1afafdd9527
Sam Bloomberg <sam@redxdev.com>
2016-03-22 13:05:36 -0400

    renamed client project

--- client/config/environment.js
+++ client/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'client',
+    modulePrefix: 'hvzsite-client',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
