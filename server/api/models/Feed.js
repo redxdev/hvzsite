08096fb0ca1fab6051b6c09835a85490243865d0
Sam Bloomberg <sam@redxdev.com>
2017-06-02 23:17:13 -0700

    basic feed model completed

--- server/api/models/Feed.js
+++ server/api/models/Feed.js
@@ -1,10 +1,51 @@
 
+/*
+Messages are in the following format:
+[
+  "This is a string with a user reference: ",
+  {t:'u',i: 123},
+  "."
+]
+
+Each part of the array is concatinated. Objects are checked
+for the 't' field, which denotes type.
+
+If the type is 'u' (user) then the i field should be present,
+which denotes the user id.
+
+If the type is 'e' (error), then the type is unknown and should
+either show an error message or be discarded.
+*/
+
 module.exports = {
 
   attributes: {
     relevantUser: {
       model: 'User'
+    },
+
+    message: {
+      type: 'array'
+    },
+
+    getMessageDebug: function () {
+      var result = "";
+      this.message.forEach(function (el) {
+        if (_.isString(el))
+          result += el;
+        else {
+          switch (el.t) {
+            default:
+              result += '@E';
+              break;
+            
+            case 'u':
+              result += '@' + el.i;
+              break;
+          }
+        }
+      });
     }
-  }
+  },
 };
 
